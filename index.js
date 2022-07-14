7288c440b7f9d8b4b47c42b905677a54c7e4909c
gazifahad <gazifahad3@gmail.com>
2022-07-14 19:33:25 +0600

    env added

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import  express from 'express';
+import dotenv from 'dotenv'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose';
 import cors from 'cors';
 import postRoutes from './routes/posts.js'
 import { MongoClient, ServerApiVersion } from 'mongodb';
-// import dotenv from 'dotenv'
+
 // require('dotenv').config()
 
 const app=express();
+dotenv.config();
 
 
 
@@ -25,7 +27,7 @@ app.use('/posts',postRoutes)
 // pw: KrmQ41GOjZh3MT0U
 
 
-const uri = 'mongodb+srv://social:KrmQ41GOjZh3MT0U@cluster0.dbg5s.mongodb.net/?retryWrites=true&w=majority';
+const uri = process.env.CONNECTION_URL;
 const PORT=process.env.PORT || 5000;
 mongoose.connect(uri,{useNewUrlParser:true,useUnifiedTopology:true})
 .then(()=>app.listen(PORT,()=>console.log(`server running on: ${PORT}`)))
--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,9 @@
 import express from 'express';
-import {getPosts,createPost,updatePost} from '../controllers/post.js'
+import {getPosts,createPost,updatePost,likePost,deletePost} from '../controllers/post.js'
 const router =express.Router();
 router.get('/', getPosts)
 router.post('/', createPost)
 router.patch('/:id', updatePost)
+router.patch('/:id/likePost',likePost)
+router.delete('/:id',deletePost)
 export default router;
