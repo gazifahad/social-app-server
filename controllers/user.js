a4f9c46588040fdff83e9446d267c802464abd6e
gazifahad <gazifahad3@gmail.com>
2022-08-01 09:29:47 +0600

    error message added

--- controllers/user.js
+++ controllers/user.js
@@ -23,7 +23,7 @@ export const signup=async (req,res)=>{
    try {
       const existingUser=await User.findOne({email});
     if(existingUser) return res.status(400).json({message: 'user already exists!'});
-    if(password!== confirmPassword) return res.status(400).json({message: 'passwords donot match!'});
+    if(password!== confirmPassword) return res.status(400).json({message: 'passwords do not match! please use same password'});
 
     const hashedPassword=await bcrypt.hash(password,12);
     const result=await User.create({email,password:hashedPassword, name: `${firstName} ${lastName}`});
